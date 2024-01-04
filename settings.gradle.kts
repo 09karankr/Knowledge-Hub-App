ce086cb5f2f8d2bacf1aa2a1abb5b894f002b713
Karan Kumar <officialkaran39@gmail.com>
2024-01-04 13:15:56 +0530

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Knowledge hub app"
+include(":app")
+ 
