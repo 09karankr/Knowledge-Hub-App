7e1edba8287f666b49f5652cf92770cb8e3866e6
Karan Kumar <officialkaran39@gmail.com>
2024-01-04 23:50:41 +0530

    first screen complete

--- app/src/main/java/com/example/knowledgehubapp/ResourceScreen2.kt
+++ app/src/main/java/com/example/knowledgehubapp/ResourceScreen2.kt
@@ -3,11 +3,9 @@ package com.example.knowledgehubapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ResourceScreen2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_resource_screen2)
     }
 }
